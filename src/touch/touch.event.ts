9df156307118958e879fc250787df84752412631
Michael Strobel <michael@kryops.de>
2019-10-14 17:46:38 +0200

    touch: If browser supports pointer events, do not add other event listeners

--- src/touch/touch.event.ts
+++ src/touch/touch.event.ts
@@ -2,6 +2,32 @@ import {Inject, Injectable} from "@angular/core";
 import {DOCUMENT} from "@angular/common";
 import {MyEventManagerPlugin} from "../__util/event-manager-plugin";
 
+function getNativeEventNames(eventName: string): string[] {
+    // iOS 13 supports both touch and pointer events,
+    // and fires both even if we call preventDefault()
+    const supportsPointerEvents = typeof(window) !== undefined && 'PointerEvent' in window;
+
+    switch (eventName) {
+        case 'up':
+            return supportsPointerEvents
+                ? ['pointerup']
+                : ['mouseup', 'touchend'];
+
+        case 'down':
+            return supportsPointerEvents
+                ? ['pointerdown']
+                : ['mousedown', 'touchstart'];
+
+        case 'move':
+            return supportsPointerEvents
+                ? ['pointermove']
+                : ['mousemove', 'touchmove'];
+
+        default:
+            return [];
+    }
+}
+
 /**
  * Quick-firing 'up' and 'down' events that work cross-browser for mouse and touch events
  *
@@ -21,15 +47,7 @@ export class TouchEventPlugin extends MyEventManagerPlugin {
     }
 
     addEventListener(element: HTMLElement, eventName: string, handler: Function): Function {
-
-        let eventNames = ['mouseup', 'touchend', 'pointerup'];
-        if (eventName === 'move') {
-            eventNames = ['mousemove', 'touchmove', 'pointermove'];
-        } else if (eventName === 'down') {
-            eventNames = ['mousedown', 'touchstart', 'pointerdown'];
-        }
-
-        const eventListeners = eventNames.map(x =>
+        const eventListeners = getNativeEventNames(eventName).map(x =>
             this.manager.addEventListener(element, x, (e: any) => {
                 // prevent default so only one of the event listeners is fired
                 e.preventDefault();
