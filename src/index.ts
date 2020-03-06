2d90f0c5252b82266493c383671beee5edde95b7
Michael Strobel <michael@kryops.de>
2020-03-06 22:42:47 +0100

    Update to Angular 9

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,12 @@ export {OutsideEventModule} from "./outside/index";
 export {UndetectedEventModule} from "./undetected/index";
 export {MultiEventModule} from "./multi/index";
 export {ObserveEventModule} from "./observe/index";
+export {ObserveEventDirective} from "./observe-directive/observe-event.directive";
 export {ObserveEventDirectiveModule} from "./observe-directive/index";
 export {TouchEventModule} from "./touch/index";
 export {OnceEventModule} from "./once/index";
 export {ConditionEventDirectiveModule} from "./condition-directive/index";
+export {ConditionEventDirective} from "./condition-directive/condition-event.directive";
 export {ScrollEventModule, SCROLL_EVENT_TIME} from "./scroll/index";
 
 export {Ng2EventsModule}
